a262e4cfc0c2e954901366ba50a9a61f3e1cf83d
Elephantprime <jkksr5@gmail.com>
2025-08-15 19:25:25 -0400

    Update firebase.js

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -1,17 +1,16 @@
-// /public/js/firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getAuth }        from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 import { getFirestore }   from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 import { getStorage }     from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC4K7iCqvxTo6Gj5oIPsErF_vMDIh0znE",
+  apiKey: "AIzaSyDHqhU5fDxG-GW2hfUVrcHCCxBzPdHd5-M",
   authDomain: "unhinged-8c6da.firebaseapp.com",
   projectId: "unhinged-8c6da",
-  storageBucket: "unhinged-8c6da.appspot.com",
+  storageBucket: "unhinged-8c6da.firebasestorage.app",
   messagingSenderId: "248472796860",
-  appId: "1:248472796860:web:1d7488b03935ae645dab9",
-  measurementId: "G-QEEY24M17T"
+  appId: "1:248472796860:web:9d8d043e7fb051acf5dab9",
+  measurementId: "G-6BC5YCV33Q"
 };
 
 const app     = initializeApp(firebaseConfig);
